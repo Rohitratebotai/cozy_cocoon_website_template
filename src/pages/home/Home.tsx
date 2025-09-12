d551dc22a0607e7c771dc61279a9bab7300bc24c
Rohit Gound <rohit.gound@ratebotai.com>
2025-09-12 16:53:50 +0530

    initial commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,12 @@
-import Parallax from "../../components/commonComponents/parallax/Parallax"
+// import Parallax from "../../components/commonComponents/parallax/Parallax"
 // import Amenities from "../../components/homepage_components/amenities/Amenities"
 // import Homepage_ExclusiveService from "../../components/homepage_components/homepage_exclusiveservice/Homepage_ExclusiveService"
-import Homepage_LetUsGuide from "../../components/homepage_components/homepage_letusguide/Homepage_LetUsGuide"
+// import Homepage_LetUsGuide from "../../components/homepage_components/homepage_letusguide/Homepage_LetUsGuide"
 import Homepage_Properties from "../../components/homepage_components/homepage_Properties/Homepage_Properties"
-import Homepage_Testimonial from "../../components/homepage_components/homepage_testimonial/Homepage_Testimonial"
-import Homepage_WhyChoose from "../../components/homepage_components/homepage_whychoose/Homepage_WhyChoose"
+// import Homepage_Testimonial from "../../components/homepage_components/homepage_testimonial/Homepage_Testimonial"
+// import Homepage_WhyChoose from "../../components/homepage_components/homepage_whychoose/Homepage_WhyChoose"
 // import Slider from "../../components/homepage_components/slider/Slider"
-import img from '../../assets/cosy_villa_images/img_19.jpeg'
+// import img from '../../assets/cosy_villa_images/img_19.jpeg'
 import Team from "../../components/homepage_components/team/Team"
 // import HomePage_Locations from "../../components/homepage_components/homepage_locations/HomePage_Locations"
 // import CounterUp from "../../components/homepage_components/counterup/CounterUp"
